80c86b010a70d8377cb7ff646f58da76ad0aebe3
Md Saiful Islam <saiful.sgd@gmail.com>
2024-12-19 16:48:20 +0600

    Demo project (small)

---
+++ custom.js
@@ -0,0 +1,11 @@
+     "use strict"
+
+function fai() {
+     document.querySelector('.bodyAll').style.display='block';
+     document.querySelector('.popUp').style.display='block';
+}
+
+function sai() {
+     document.querySelector('.bodyAll').style.display='none';
+     document.querySelector('.popUp').style.display='none';
+}
